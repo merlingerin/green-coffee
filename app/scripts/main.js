ee0bbf754e1258d6b6c970edfec5c6408b85c364
s.hvozd <s.hvozd@devcaz.com>
2018-11-10 17:48:30 +0100

    added animaton for elements

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,41 +1,81 @@
-const swiper = new Swiper(".swiper-container", {
-    freeMode: true,
-    setWrapperSize: true,
-    direction: "horizontal",
-    slidesPerView: "auto",
-    simulateTouch: false,
-    resistance: true,
-    resistanceRatio: 0,
-    mousewheel: {
-        sensitivity: 1
-    },
-    breakpoints: {
-        767: {
-            direction: "vertical"
-        }
-    }
-});
-
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener("resize", () => {
-    swiper.update();
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
-new jBox("Modal", {
-    attach: ".button__to-order",
-    title: "Заполните форму и мы свяжемся с вами",
-    content:
-        '<div class="modal__content"><form action="#" id="modal-form" method="post"><input type="text" id="input-name" name="name" placeholder="Имя"><input type="tel" id="input-tel" name="tel" placeholder="Телефон"><input type="submit" value="Отправить"></form></div>'
-});
-
 $("ready", () => {
+    var textAnimation = function() {
+        const words = $(".swiper-slide-active .ml12");
+
+        if (!words.hasClass("activated")) {
+            words.map((key, item) => {
+                $(item).wrap('<span class="new"></span>');
+                TweenLite.to(item, 0.1, { opacity: 1 });
+                TweenLite.to(item, key / 3 + 0.5, { right: 0 });
+            });
+            words.addClass("activated");
+        }
+    };
+
+    const swiper = new Swiper(".swiper-container", {
+        freeMode: true,
+        setWrapperSize: true,
+        direction: "horizontal",
+        slidesPerView: "auto",
+        simulateTouch: false,
+        resistance: true,
+        resistanceRatio: 0,
+        mousewheel: {
+            sensitivity: 1
+        },
+        breakpoints: {
+            767: {
+                direction: "vertical"
+            }
+        }
+    });
+
+    swiper.on("slideChange", function() {
+        setTimeout(() => {
+            textAnimation();
+        }, 500);
+    });
+    swiper.on("slideNextTransitionStart", function() {
+        console.log("Hey");
+    });
+
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    // We listen to the resize event
+    window.addEventListener("resize", () => {
+        swiper.update();
+        // We execute the same script as before
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+
+    new jBox("Modal", {
+        attach: ".button__to-order",
+        title: "Заполните форму и мы свяжемся с вами",
+        content:
+            '<div class="modal__content"><form action="#" id="modal-form" method="post"><input type="text" id="input-name" name="name" placeholder="Имя"><input type="tel" id="input-tel" name="tel" placeholder="Телефон"><input type="submit" value="Отправить"></form></div>',
+        onCreated: function() {
+            const inputName = $("#input-name");
+            const inputTel = $("#input-tel");
+            inputName.add(inputTel).on("input", e => {
+                const target = e.target;
+                const value = target && target.value;
+                console.log("value", value);
+                if (value && value.length > 0) {
+                    return $(target).addClass("filled");
+                }
+                return $(target).removeClass("filled");
+            });
+        }
+    });
+
+    /**
+     * Preloader
+     */
     try {
         var decoration = document.getElementById("logo__decoration");
         var logLink = document.getElementById("logo__link");
@@ -66,7 +106,8 @@ $("ready", () => {
         var tpreloader = new TimelineMax();
 
         var onLoaded = function() {
-            tpreloader.to(preloader, 1, { css: { scaleX: 0, xPercent: 100 }, onComplete: textAnimation });
+            tpreloader.to(preloader, 1, { css: { scaleX: 0.5, xPercent: 60, opacity: 0 }, onComplete: textAnimation });
+            tpreloader.to(preloader, 0.1, { css: { display: "none" } });
         };
         tl.to(decoration, 1, { xPercent: 0 });
         tl.to(decoration, 1, { xPercent: 100 });
@@ -84,71 +125,5 @@ $("ready", () => {
             preloader.style.display = "none";
         }
     }
-
-    var textAnimation = function() {
-        const words = $(".ml12");
-        const words1 = $("#hero-title");
-
-        words.map((key, item) => {
-            $(item).wrap('<i class="new"></i>');
-            TweenLite.to(item, 0.1, { opacity: 1 });
-            TweenLite.to(item, key / 3 + 0.5, { right: 0 });
-        });
-    };
-
-    // Wrap every letter in a span
-
-    $("load", function() {
-        // $(".ml12").each(function() {
-        //     $(this).html(
-        //         $(this)
-        //             .text()
-        //             .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
-        //     );
-        // });
-        // anime.timeline({ loop: false }).add({
-        //     targets: ".ml12 .letter",
-        //     translateX: [0, 140],
-        //     translateZ: 0,
-        //     opacity: [0, 1],
-        //     easing: "easeOutExpo",
-        //     duration: 5000,
-        //     delay: function(el, i) {
-        //         return 2500 + 30 * i;
-        //     }
-        // });
-    });
-    // $(".ml12").each(function() {
-    //     $(this).html(
-    //         $(this)
-    //             .text()
-    //             .replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>")
-    //     );
-    // });
-
-    // const textAnimation = function() {
-    //     anime.timeline({ loop: false }).add({
-    //         targets: ".ml12 .letter",
-    //         translateX: [40, 0],
-    //         translateZ: 0,
-    //         opacity: [0, 1],
-    //         easing: "easeOutExpo",
-    //         duration: 800,
-    //         delay: function(el, i) {
-    //             return 500 + 30 * i;
-    //         }
-    //     });
-
-    // anime.timeline({ loop: true }).add({
-    //     targets: ".ml12 .letter",
-    //     translateX: [40, 0],
-    //     translateZ: 0,
-    //     opacity: [0, 1],
-    //     easing: "easeOutExpo",
-    //     duration: 1200,
-    //     delay: function(el, i) {
-    //         return 500 + 30 * i;
-    //     }
-    // });
-    // };
+    // ============================================================
 });
